f6aee3b6728f3e575465ce157a869ccebd45ef62
= <chu.andy1993@gmail.com>
2019-08-11 10:26:15 -0700

    copy_list_with_random_pointer.py code refactored to be more clean

--- ds_and_alg/copy_list_with_random_pointer.py
+++ ds_and_alg/copy_list_with_random_pointer.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+
 """
 # Definition for a Node.
 class Node:
@@ -9,22 +10,18 @@ class Node:
 """
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        curr = head
+        curr = head 
         hashMap = defaultdict(lambda: None)
-        while (curr):
+        while (curr): 
             if curr not in hashMap: 
                 hashMap[curr] = Node(curr.val, None, None)
             if curr.next != None: 
                 if curr.next not in hashMap: 
                     hashMap[curr.next] = Node(curr.next.val, None, None)
                 hashMap[curr].next = hashMap[curr.next]
-            else: 
-                hashMap[curr].next = None
             if curr.random != None: 
                 if curr.random not in hashMap: 
                     hashMap[curr.random] = Node(curr.random.val, None, None)
                 hashMap[curr].random = hashMap[curr.random]
-            else: 
-                hashMap[curr].random = None
             curr = curr.next
         return hashMap[head]
